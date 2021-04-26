7269ec71b90dc70b21a05fc8ebc061f94c666f27
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-04-27 05:25:29 +0800

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-// like import express from 'express';(ES2015)
+const express = require('express'); // nodeJS 基本上還是採用commonJS的寫法
+// 就算寫 import express from 'express';(ES2015) 也還是需要編譯
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport'); // 因為該檔案沒有export, 所以只需要執行該程式
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,// cookie的時效，並且要轉換為毫秒(此範例為30天)
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ services/passport.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  // 這裡的第一個參數 - user 是來自下方的 done 裡面的第二個參數
+  // 也就是資料庫裏面的單一 user 資料
+  // 不使用 google 的個人 ID 是因為用戶不一定是使用 google (也許是FB)
+  done(null, user.id);
+  // 這邊的 user.id 指的不是 google 用戶的id
+  // 這邊的 id 是使用 mongoDB 時，自動分配給此物件的 id
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    // 這裡要從 cookie 裡面拿出 id 然後拿去給 server 比較
+    // 所以使用的是 model class -- User
+    .then((user) => {
+      done(null, user);
+    })
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+      // 尋找是否有相符的ID
+      .then((existingUser) => {
+        if (existingUser) {
+          // 我們已有此使用者資料
+          done(null, existingUser);
+          // 第一個參數是錯誤訊息的Object，然而在此範例中
+          // 我們不關心錯誤訊息， 所以使用 null
+        } else {
+          // 我們沒有此使用者的資料，建立一位新的使用者
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      })
+  })
+);
