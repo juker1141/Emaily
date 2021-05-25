489bdc02ec306f4bbb625816e29d53f5398592a2
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-05-25 08:46:29 +0800

    try to fix tailwindcss-3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   purge: {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    options: {
-      safelist: ['red', 'yellow', 'green', '400', '500', '600', '700', '800']
-    }
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
