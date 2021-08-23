2d7ac3d1a362e753cde29238f2557e12cb66d70e
juker1141 <50107434+juker1141@users.noreply.github.com>
2021-08-23 10:39:12 +0800

    Update surveyTemplate.js

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -5,12 +5,19 @@ module.exports = (survey) => {
     <html>
       <body>
         <div style="text-align: center;">
-          <h3>I'd like your input!</h3>
-          <p>Please answer the following question:</p>
           <p>${survey.body}</p>
-          <div style="display:">
-            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a>
-            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
+          <div>
+            <a
+              style="margin-right: 24px"
+              href="${keys.redirectDomain}/api/surveys/${survey.id}/yes"
+            >
+              Yes
+            </a>
+            <a
+              href="${keys.redirectDomain}/api/surveys/${survey.id}/no"
+            >
+              No
+            </a>
           </div>
         </div>
       </body>
